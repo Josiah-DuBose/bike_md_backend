fd2e7ff3baf5dfa581ed063a861c9b3ce3d281fd
Josiah DuBose <josiah7686@gmail.com>
2018-05-26 14:32:00 -0400

    DB populating script

--- api_retrieve.py
+++ api_retrieve.py
@@ -12,16 +12,15 @@ def dbConnect():
     return conn
 
 
-def getURLs(**args):
-    print("getURLs")
-    print(args)
+def getURL(**args):
     """Return URL for given action and params"""
     #Instantiate url string
     url = ''
+
     if (args["action"] == "makes"):
         url="https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/Motorcycle?format=json"
     if (args["action"] == "models"):
-        url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/"
+        url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make"
 
     return url
 
@@ -36,13 +35,62 @@ def tableCleanUp(**args):
     conn.commit()
 
 
-def updateModels(brands):
+def updateModels(makes):
+    print("updateModels")
     ###Retrieve models and store them in the DB###
 
     #DB connect and cursor creation
     conn = dbConnect()
     cur = conn.cursor()
 
+    #DB house keeping
+    tableCleanUp(conn=conn, name="diag_app_model")
+
+    #Get all the years from the DB
+    cur.execute("""SELECT * from diag_app_year;""")
+    rows = cur.fetchall()
+
+    baseURL = getURL(action="models")
+    for row in rows:
+        for make in makes:
+            #Get all models for each make
+            models = []
+            try:
+                #Build URL and make request 
+                url = "{}/{}/modelyear/{}?format=json".format(baseURL,make["brand"],row[1])
+                # print("ULR: {}").format(url)
+                response = requests.get(url)
+
+                #Skip if no models or less then 5  models found for make
+                if (response.json()["Count"] == 0 or response.json()["Count"] < 5): 
+                    continue
+
+                #Terminal output
+                print("Year: {} adding {} models for {}".format(row[1], response.json()["Count"], make["brand"]))
+                models = response.json()["Results"]
+
+            except:
+                continue
+
+            #Insert all models for make into DB
+            for model in models:
+                name = model["Model_Name"]
+
+                #If we get a really long name move on to the next iteration 
+                if (len(name) > 20):
+                    continue
+
+                #Clean up name
+                clean_name = re.sub('\'', '', name)
+
+                #Insert into DB
+                sql = """INSERT INTO diag_app_model (name, brand_id, year_id) 
+                VALUES ('{}', '{}', '{}')""".format(clean_name, make["id"], row[0])
+                try:
+                    cur.execute(sql)
+                except psycopg2.Error as e:
+                    print(e.pgerror)
+                conn.commit() 
 
 def updateMakes():
     ###Retrieve makes and store them in the DB###
@@ -51,9 +99,15 @@ def updateMakes():
     conn = dbConnect()
     cur = conn.cursor()
 
+    all_makes = {}
+    make_response_array = []
+
     #Call external API for current make data
-    all_makes = requests.get(getURLs(action="makes"))
-    make_response_array = all_makes.json()["Results"]
+    try:
+        all_makes = requests.get(getURL(action="makes"))
+        make_response_array = all_makes.json()["Results"]
+    except:
+        print(all_makes)
 
     #DB house keeping
     tableCleanUp(conn=conn, name="diag_app_brand")
@@ -69,8 +123,8 @@ def updateMakes():
         #Clean up name
         clean_name = re.sub('\'', '', name)
         
-        #If we get a really name move on to the next iteration 
-        if (len(clean_name) > 15):
+        #If we get a really long name or name shorter then 3 move on to the next iteration 
+        if (len(clean_name) > 15 or len(clean_name) < 3):
             continue
 
         #Build statement and insert 
